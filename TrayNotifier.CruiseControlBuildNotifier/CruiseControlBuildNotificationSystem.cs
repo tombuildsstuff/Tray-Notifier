4eced33fa1e4353558305e1862063607a7ec2a02
Tom Harvey <tom@ibuildstuff.co.uk>
2011-11-17 23:36:42 +0000

    started adding in a cruise control build notifier

---
+++ TrayNotifier.CruiseControlBuildNotifier/CruiseControlBuildNotificationSystem.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using TrayNotifier.Business.Helpers;
+using TrayNotifier.CruiseControlBuildNotifier.Models;
+
+namespace TrayNotifier.CruiseControlBuildNotifier
+{
+    using Business;
+
+    public class CruiseControlBuildNotificationSystem : AbstractNotificationSystem
+    {
+        private readonly WebClient _webClient;
+        private readonly string _cruiseControlInstallUrl;
+        private readonly int _numSecondsToCheck;
+        private bool _isDownloading;
+        private List<CruiseControlProject> _currentProjects;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CruiseControlBuildNotificationSystem"/> class.
+        /// </summary>
+        /// <param name="cruiseControlInstallUrl">
+        /// The Cruise Control URL i.e. "http://build.mycompany.com/ccnet/"
+        /// </param>
+        /// <param name="numSecondsToCheck">
+        /// The interval used to check the build status at; defaults to 90
+        /// </param>
+        /// <param name="credentials">
+        /// The credentials used to connect to the build URL
+        /// </param>
+        public CruiseControlBuildNotificationSystem(string cruiseControlInstallUrl, int numSecondsToCheck = 90, ICredentials credentials = null)
+        {
+            _cruiseControlInstallUrl = cruiseControlInstallUrl;
+            _numSecondsToCheck = numSecondsToCheck;
+
+            _webClient = new WebClient { Credentials = credentials, CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore) };
+            _webClient.DownloadDataCompleted += Data_Downloaded;
+        }
+
+        public override int NumberOfSecondsToCheck()
+        {
+            return _numSecondsToCheck;
+        }
+
+        public override void Check()
+        {
+            if (_isDownloading)
+                return;
+
+            _isDownloading = true;
+            _webClient.DownloadDataAsync(new Uri(string.Format("{0}/ViewFarmReport.aspx/XmlServerReport.aspx", _cruiseControlInstallUrl)));
+        }
+
+        private void Data_Downloaded(object sender, DownloadDataCompletedEventArgs e)
+        {
+            _isDownloading = false;
+            
+            if (e.Error != null)
+                throw e.Error;
+
+
+            var xml = e.Result.ConvertToString();
+            var newProjects = CruiseControlProject.ParseCruiseControlExportToListOfProjects(xml);
+            var changedProjects = new List<CruiseControlProject>();
+
+            var initialLoad = _currentProjects.Count == 0;
+            foreach (var project in newProjects)
+            {
+                var existing = _currentProjects.Where(p => p.Name == project.Name).FirstOrDefault();
+                if (existing == null || (existing.LastBuildStatus != project.LastBuildStatus) || (existing.LastBuildTime != project.LastBuildTime))
+                    changedProjects.Add(project);
+            }
+            _currentProjects = newProjects;
+
+            var successful = changedProjects.Where(cp => cp.LastBuildStatus == "Failed").OrderBy(p => p.Name).ToList();
+            var failed = changedProjects.Where(cp => cp.LastBuildStatus == "Successful").OrderBy(p => p.Name).ToList();
+            var unknown = changedProjects.Where(cp => !successful.Any(s => s.Name == cp.Name) && !failed.Any(f => f.Name == cp.Name)).ToList();
+
+            if (!initialLoad)
+                foreach (var project in unknown)
+                    if (MessageReceived != null)
+                        MessageReceived(10000, "Oooh look at you doing some work!", string.Format("{0} is a new project..", project.Name), Icon.Warning);
+
+            foreach (var project in failed)
+                if (MessageReceived != null)
+                    MessageReceived(20000,
+                                    string.Format("{0} is Broken", project.Name),
+                                    string.Format("Broke At {0} {1}", project.LastBuildTime.Value.ToLongDateString(), project.LastBuildTime.Value.ToShortTimeString()),
+                                    Icon.Error);
+
+            foreach (var project in successful)
+                if (MessageReceived != null)
+                    MessageReceived(6000,
+                                    string.Format("{0} Build Again", project.Name),
+                                    string.Format("Built At {0} {1}", project.LastBuildTime.Value.ToLongDateString(), project.LastBuildTime.Value.ToShortTimeString()),
+                                    Icon.Info);
+        }
+    }
+}
---
+++ TrayNotifier.CruiseControlBuildNotifier/Models/CruiseControlProject.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace TrayNotifier.CruiseControlBuildNotifier.Models
+{
+    using System;
+    using System.Collections.Generic;
+    public class CruiseControlProject
+    {
+        public string Name { get; set; }
+
+        public string LastBuildStatus { get; set; }
+
+        public DateTime? LastBuildTime { get; set; }
+
+        public string WebUrl { get; set; }
+
+        /// <summary>
+        /// Parses a Cruise Control export 'Project' node into a CruiseControlProject object
+        /// </summary>
+        /// <param name="element">XElement object</param>
+        /// <returns>Cruise Control Project</returns>
+        public static CruiseControlProject ParseCruiseControlExportToCruiseControlProject(XElement element)
+        {
+            if (element == null)
+                return null;
+
+            var project = new CruiseControlProject();
+
+            var name = element.Attribute("name");
+            if (name != null)
+                project.Name = name.Value;
+
+            var lastBuildStatus = element.Attribute("lastBuildStatus");
+            if (lastBuildStatus != null)
+                project.LastBuildStatus = lastBuildStatus.Value;
+
+            var lastBuildTime = element.Attribute("lastBuildTime");
+            DateTime date;
+            if (lastBuildTime != null && DateTime.TryParse(lastBuildTime.Value, out date))
+                project.LastBuildTime = date;
+
+            var url = element.Attribute("webUrl");
+            if (url != null)
+                project.WebUrl = url.Value;
+
+            return project;
+        }
+
+        /// <summary>
+        /// Parses an Xml Document into a list of Cruise Control Project objects
+        /// </summary>
+        /// <param name="xml">Xml Document from the Cruise Control Site</param>
+        /// <returns>List of Cruise Control Projects</returns>
+        public static List<CruiseControlProject> ParseCruiseControlExportToListOfProjects(string xml)
+        {
+            var document = XDocument.Parse(xml);
+            return document.Descendants("Projects")
+                           .Descendants("Project")
+                           .Select(ParseCruiseControlExportToCruiseControlProject)
+                           .ToList();
+        }
+    }
+}
